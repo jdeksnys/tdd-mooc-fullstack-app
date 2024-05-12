e5a549344a800f1f33f9e2c3ed98616c83dc8ab1
jdeksnys <jonasdeksnys@gamil.com>
2024-05-12 12:08:50 +0300

    express dependency

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-const http = require('http');
+const http = require('http')
+const express = require('express')
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
 const PORT = 3001

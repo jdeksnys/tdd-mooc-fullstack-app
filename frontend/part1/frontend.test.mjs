4811b57b2f420f7f2b51e8187e4a615c6ebf8fa5
jdeksnys <jonasdeksnys@gamil.com>
2024-05-12 21:35:57 +0300

    setup frontend testing

---
+++ frontend/part1/frontend.test.mjs
@@ -0,0 +1,7 @@
+import { expect, afterAll, test, describe, afterEach } from 'vitest';
+
+describe("Frontend tests", () => {
+    test("example", async () => {
+        expect(1).to.equal(1);
+    });   
+});
